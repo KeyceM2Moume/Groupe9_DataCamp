7d895cb815998831e89bacea515c3cdf0377f505
KeyceM2Moume <walteramasenpai@gmail.com>
2024-11-27 17:16:54 +0100

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+git init
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git remote add https://github.com/KeyceM2Moume/Groupe9_DataCamp
+git push -u origin main
+pip install --upgrade pip
 pip install -r requirements.txt
+pip freeze
