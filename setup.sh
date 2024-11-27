7643414e52e687e4397155ae9e28ee1345ec4eac
KeyceM2Moume <walteramasenpai@gmail.com>
2024-11-27 17:23:12 +0100

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+pip install --upgrade pip
+rustc --version
+pip install --no-cache-dir -r requirements.txt
+pip install -r requirements.txt
+pip freeze
+pip install tokenizers
 git init
 git add .
 git commit -m "Initial commit"
 git branch -M main
 git remote add https://github.com/KeyceM2Moume/Groupe9_DataCamp
 git push -u origin main
-pip install --upgrade pip
-pip install -r requirements.txt
-pip freeze
-pip install tokenizers
-pip install --no-cache-dir -r requirements.txt
-rustc --version
+
+
