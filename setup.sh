7f5970291f6fbcadcb20290e2bccde75c04b6d21
KeyceM2Moume <walteramasenpai@gmail.com>
2024-11-27 17:26:00 +0100

    setup.sh

--- setup.sh
+++ setup.sh
@@ -3,13 +3,6 @@ pip install --upgrade pip
 rustc --version
 pip install --no-cache-dir -r requirements.txt
 pip install -r requirements.txt
-pip freeze
 pip install tokenizers
-git init
-git add .
-git commit -m "Initial commit"
-git branch -M main
-git remote add https://github.com/KeyceM2Moume/Groupe9_DataCamp
-git push -u origin main
 
 
